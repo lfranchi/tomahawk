8e40c84516ad87804dcd7e67b47a27e3a84c7f98
Leo Franchi <lfranchi@kde.org>
2011-09-17 12:23:31 -0400

    Add spotify support to jobsview

--- src/tomahawkwindow.h
+++ src/tomahawkwindow.h
@@ -56,7 +56,6 @@ public:
 
     AudioControls* audioControls() { return m_audioControls; }
     SourceTreeView* sourceTreeView() const { return m_sourcetree; }
-    JobStatusModel* jobsModel() const { return m_jobsModel; }
 
     void setWindowTitle( const QString& title );
 
